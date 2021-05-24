a57cdd604ca8968c274e819e5dc590b21fc60687
chuhelan <chuhelan@163.com>
2021-05-24 11:39:36 +0800

    :tada: Begin a project !

---
+++ src/main/java/com/chuhelan/netex/test/Test.java
@@ -0,0 +1,13 @@
+package com.chuhelan.netex.test;
+
+/**
+ * @description:
+ * @author: chuhelan
+ * @create: 2021-05-24 11:35
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
