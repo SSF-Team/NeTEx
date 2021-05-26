3c5f0715ab26285fd26aa042114eb29a717eea92
Stapxs <1007028430.stapx@gmail.com>
2021-05-26 20:31:44 +0800

    :ambulance: Recover files deleted by mistake

--- src/main/java/com/chuhelan/netex/controller/UserController.java
+++ src/main/java/com/chuhelan/netex/controller/UserController.java
@@ -11,10 +11,7 @@ import com.chuhelan.netex.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
