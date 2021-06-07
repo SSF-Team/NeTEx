60f96abe03831f7aab6b7c392cd099949ff3f416
chuhelan <chuhelan@163.com>
2021-06-07 10:55:22 +0800

    add file upload

--- src/main/java/com/chuhelan/netex/controller/FileUploadController.java
+++ src/main/java/com/chuhelan/netex/controller/FileUploadController.java
@@ -1,63 +1,71 @@
 package com.chuhelan.netex.controller;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 /**
  * @description:
  * @author: chuhelan
- * @create: 2021-05-31 13:51
+ * @create: 2021-05-31 13:47
  **/
 
 @Controller
 public class FileUploadController {
-    //@RequestParam("uploadfile") 将name=file控件得到的文件封装成CommonsMultipartFile 对象
+    @RequestMapping("fileupload")
+    public String fileupload(HttpServletRequest request) throws Exception {
+        System.out.println("文件上传...");
 
-    @RequestMapping("/fileUpload")
-    public String handleFormUpload(@RequestParam("name") String name,
-                                   @RequestParam("uploadfile") List<MultipartFile> uploadfile,
-                                   HttpSession session,
-                                   Model model) {
-        List<String> fileList = new ArrayList<>();
-        if (!uploadfile.isEmpty() && uploadfile.size() > 0) {
-            for (MultipartFile file : uploadfile) {
-//获得上传文件的原始名称
-                String orginalFilename = file.getOriginalFilename();
-                System.out.println("长传文件原始名称：" + orginalFilename);
-//设置上传文件的保存地址目录
-                String
-                        realPath = session.getServletContext().getRealPath("/upload");
-                System.out.println("realPath:" + realPath);
-//采用uuid重命名上传文件；格式为：上传人_uuid_原始文件名
-                String newFilename = name + "_" +
-                        UUID.randomUUID() + "_" + orginalFilename;
-                fileList.add(newFilename);
-                File destFile = new File(realPath + "\\" + newFilename);
-//此处用来看路径是否正确
-                System.out.println("destFile:" + destFile);
-//使用MultipartFile接口的方法完成文件上传到指定位置
-                try {
-                    file.transferTo(destFile);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    return "error";
-                }
-            }
-            model.addAttribute("name", name);
-            model.addAttribute("fileList", fileList);
-            return "showUpload";
-        } else {
-            return "error";
+        // 使用fileupload组件完成文件上传
+        // 上传的位置
+        String path = request.getSession().getServletContext().getRealPath("/uploads/");
+        // 判断，该路径是否存在
+        File file = new File(path);
+        if (!file.exists()) {
+            // 创建该文件夹
+            file.mkdirs();
         }
+
+        // 解析request对象，获取上传文件项
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        // 解析request
+        List<FileItem> items = upload.parseRequest(request);
+        // 遍历
+        for (FileItem item : items) {
+            // 进行判断，当前item对象是否是上传文件项
+            if (item.isFormField()) {
+                // 说明普通表单向
+            } else {
+                // 说明上传文件项
+                // 获取上传文件的名称
+                String filename = item.getName();
+                // 把文件的名称设置唯一值，uuid
+                String uuid = UUID.randomUUID().toString().replace("-", "");
+                filename = uuid + "_" + filename;
+                // 完成文件上传
+                item.write(new File(path, filename));
+                // 删除临时文件
+                item.delete();
+            }
+        }
+
+        return "api";
+    }
+
+    @RequestMapping("file")
+    public String filePage(){
+        return "file";
     }
 }
